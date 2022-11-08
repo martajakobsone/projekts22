58d19819a0bd2becb91e4b6e1c4d381cefbcdd70
martajakobsone <98958764+martajakobsone@users.noreply.github.com>
2022-11-08 11:47:14 +0200

    Update funkcijas.js

--- funkcijas.js
+++ funkcijas.js
@@ -1,3 +1,36 @@
 function funkcija(){
     document.write("Viss")
 }
+function radiChatu(dati){
+    var jaunaRinda = "<br/>"
+    var chats = "" ;
+    var vieta = document.getElementById("chats")
+    for (var rinda of dati["chats"]){
+        chats += rinda + jaunaRinda
+    }
+    vieta.innerHTML = chats
+}
+
+async function lasi(){
+    const atbilde = await fetch('/chats/lasi')
+    const datuObj = await atbilde.json()
+    radiChatu (datuObj);
+    await new Promise (resolve => setTimeout(resolve, 5000));
+    await lasi();
+}
+
+async function suti(){
+    zinasElements = document.getElementById("zina")
+    zina = zinasElements.value
+    console.log(zina)
+    zinasElements.value = ""
+    const atbilde = await fetch('/chats/suti', {
+        method: 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({"chats" : zina})
+    })
+    const datuObj = await atbilde.json()
+    radiChatu (datuObj);
+}
